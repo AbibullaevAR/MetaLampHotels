1eb0ddbb2c0fc67282c8f51325ad2c467525df85
AbibullaevAR <a.alim2019@mail.ru>
2024-01-17 20:56:11 +0300

    add main page

---
+++ src/components/MainFiltredHotels.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useHotelStore } from '@/stores/hotel';
+import { storeToRefs } from 'pinia';
+import HotelCard from './HotelCard.vue';
+
+const hotelStore = useHotelStore()
+const { filtredHotels } = storeToRefs(hotelStore)
+
+</script>
+
+<template>
+    <div class="main-filtred-hotels">
+        <H1>Номера, которые мы для вас подобрали</H1>
+        <div class="main-filtred-hotels__wrapper">
+            <HotelCard v-for="hotel of filtredHotels" :key="hotel.id" :hotel="hotel">
+                <img :src="hotel.image" alt="">
+            </HotelCard>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.main-filtred-hotels__wrapper{
+    display: flex;
+    flex-wrap: wrap;
+    row-gap: 10px;
+    column-gap: 10px;
+    margin-top: 20px;
+}
+</style>
