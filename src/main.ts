b0ae649c3c0f6d818335f15d6b9f19e10e2e7f6a
AbibullaevAR <a.alim2019@mail.ru>
2024-01-17 20:49:35 +0300

    add settings

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import './assets/main.css'
+import 'vue-rate/dist/vue-rate.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import rate from 'vue-rate'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(rate)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import FiltersPanel from '@/components/FiltersPanel.vue';
+import MainFiltredHotels from '@/components/MainFiltredHotels.vue';
+import { ref, watch } from 'vue';
+import { useHotelStore } from '@/stores/hotel';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main-page">
+    <FiltersPanel class="main-page__filter-panel"></FiltersPanel>
+    <MainFiltredHotels class="main-page__hotels"></MainFiltredHotels>
   </main>
 </template>
+
+<style scoped>
+.main-page {
+  display: flex;
+  justify-content: center;
+  column-gap: 50px;
+}
+.main-page__filter-panel{
+  flex-shrink: 0;
+}
+.main-page__hotels{
+  flex-shrink: 0;
+  flex-basis: 835px;
+}
+</style>
+
+<style>
+body {
+  overflow-y: scroll;
+}
+</style>
