4fc3d5382cce8c027af0a25c21edb2385d66f8f5
AbibullaevAR <a.alim2019@mail.ru>
2024-01-17 20:55:59 +0300

    add filters panel

---
+++ src/components/FiltersPanel.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { DateInHotelFilter, PriceRangeFilter, RoomsRuleFilter, GuestsFilter, RoomsFilter } from '@/components/Filters';
+
+</script>
+
+<template>
+    <div class="filters-panel">
+        <DateInHotelFilter></DateInHotelFilter>
+        <GuestsFilter></GuestsFilter>
+        <PriceRangeFilter></PriceRangeFilter>
+        <RoomsRuleFilter></RoomsRuleFilter>
+        <RoomsFilter></RoomsFilter>
+    </div>
+</template>
+
+<style scoped>
+.filters-panel {
+    display: flex;
+    flex-direction: column;
+    row-gap: 21px;
+}
+</style>
