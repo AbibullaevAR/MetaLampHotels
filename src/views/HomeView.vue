94400378b0dd0bd1c0725d58b38d1d83fc5ff555
AbibullaevAR <a.alim2019@mail.ru>
2024-01-21 17:40:28 +0300

    add footer to page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
 import FiltersPanel from '@/components/FiltersPanel.vue';
 import MainFiltredHotels from '@/components/MainFiltredHotels.vue';
 import MainHeader from '@/components/MainHeader.vue';
-import { ref, watch } from 'vue';
-import { useHotelStore } from '@/stores/hotel';
+import MainFooter from '@/components/MainFooter.vue';
+
 </script>
 
 <template>
@@ -12,6 +12,7 @@ import { useHotelStore } from '@/stores/hotel';
     <FiltersPanel class="main-page__filter-panel"></FiltersPanel>
     <MainFiltredHotels class="main-page__hotels"></MainFiltredHotels>
   </main>
+  <MainFooter class="main-footer"></MainFooter>
 </template>
 
 <style scoped>
@@ -29,6 +30,9 @@ import { useHotelStore } from '@/stores/hotel';
 .main-page__hotels{
   flex-shrink: 0;
   flex-basis: 835px;
+}
+.main-footer {
+  margin-top: 50px;
 }
 </style>
 
