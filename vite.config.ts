012babb8ce6bbd57be5a0df2468d52a62bf27a76
AbibullaevAR <a.alim2019@mail.ru>
2024-01-23 10:52:41 +0300

    add viteStaticCopy for copy hotels Images to dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/hotelsImage/*',
+          dest: './assets/hotelsImage/'
+        }
+      ]
+    })
   ],
   resolve: {
     alias: {
